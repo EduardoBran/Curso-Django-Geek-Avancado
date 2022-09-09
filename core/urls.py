ff3225d058f7b06483ee9cc3e06e2acd76773923
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-09-09 18:49:50 -0300

    definindo os templates | configurando settings

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import *
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
 ]
