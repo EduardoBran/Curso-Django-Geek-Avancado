c24549bdfea94e00d7d42e57a92db88a84cb4a16
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-09-12 19:54:14 -0300

    criado models e admin para servico | cargo | funcionarios

--- core/models.py
+++ core/models.py
@@ -1,3 +1,68 @@
-from django.db import models
+import uuid  # gera valores hexadecimais aleatórios
 
-# Create your models here.
+from django.db import models
+from stdimage.models import StdImageField
+
+
+def get_file_path(_instance, filename): # necessário para gerar nomes diferentes para as imagens
+    extensao = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{extensao}'
+    return filename
+
+class Base(models.Model):
+    criados = models.DateField(verbose_name='Criação', auto_now_add=True)
+    modificado = models.DateField(verbose_name='Atualização', auto_now=True)
+    ativo = models.BooleanField(verbose_name='Ativo', default=True)
+    
+    class Meta:
+        abstract = True
+
+
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+    
+    servico = models.CharField('Serviço', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    icone = models.CharField('Ícone', max_length=20, choices=ICONE_CHOICES) # vai ser transformado em combobox no cadsatro
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+    
+    def __str__(self):
+        return self.servico
+
+
+class Cargo(Base):
+    cargo = models.CharField('Cargo', max_length=100)
+    
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+    
+    def __str__(self):
+        return self.cargo
+
+
+class Funcionario(Base):
+    imagem = StdImageField('Foto', upload_to=get_file_path, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    nome = models.CharField('Nome', max_length=100)
+    cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
+    bio = models.TextField('Bio', max_length=200)
+    facebook = models.CharField('Facebook', max_length=100, default='#')
+    twitter = models.CharField('Twitter', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+    
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+    
+    def __str__(self):
+        return self.nome
