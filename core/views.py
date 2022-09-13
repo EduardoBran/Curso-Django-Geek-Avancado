b84fd1c6aa1eac7cab8b8b54819ea4103a17a4ec
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-09-13 13:32:49 -0300

    exibindo os dados de recurso na page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from .models import Funcionario, Servico
+from .models import Funcionario, Recurso, Servico
 
 
 class IndexView(TemplateView):
@@ -8,6 +8,17 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+
         context['servicos'] = Servico.objects.order_by('?').all()  # aleatório
         context['funcionarios'] = Funcionario.objects.order_by('?').all()
+
+        # solução simples
+        # context['recursos1'] = Recurso.objects.all()[:3]
+        # context['recursos2'] = Recurso.objects.all()[3:6]
+
+        # solução elegante
+        recursos = Recurso.objects.all()
+        context['recursosLeft'] = recursos[0:int(len(recursos) / 2)]
+        context['recursosRight'] = recursos[int(len(recursos) / 2):]
+
         return context
