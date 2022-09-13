2d6faf1f017f77d0dcac92fb3fea0aeab0fccb80
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-09-13 12:20:22 -0300

    exibindo os dados na page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
 
+from .models import Funcionario, Servico
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()  # aleatório
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        return context
